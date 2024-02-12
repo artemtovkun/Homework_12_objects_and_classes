97d3e3c66811ab4a5eeff78ad0e0411eb3e1e711
artem <artem.tovkun@gmail.com>
2024-02-12 22:13:53 +0300

    HW_12_v2

--- src/Author.java
+++ src/Author.java
@@ -18,19 +18,6 @@ public class Author {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Author author = (Author) o;
-        return Objects.equals(firstName, author.firstName) && Objects.equals(lastName, author.lastName);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(firstName, lastName);
-    }
-
-    @Override
     public String toString() {
         return "firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'';
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Author author = new Author("Андрей","Моргунов");
+
         System.out.println(author);
 
         System.out.println("========");
